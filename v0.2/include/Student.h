813237cb1164a413cd39d760db1b4203302c23df
adeolaibrahim <adeolaibrahim25@gmail.com>
2026-01-11 02:08:58 +0200

    Update Student.h

--- v0.2/include/Student.h
+++ v0.2/include/Student.h
@@ -1,3 +1,4 @@
+// Student.h
 #ifndef STUDENT_H
 #define STUDENT_H
 
@@ -9,23 +10,18 @@ public:
     std::string name;
     std::string surname;
     std::vector<double> homeworks;
-    double exam = 0.0;
+    double exam;
 
-    Student() = default;
-    Student(const std::string& n, const std::string& s, const std::vector<double>& hw, double e);
+    Student();
+    Student(const std::string& n, const std::string& s,
+            const std::vector<double>& hw, double e);
+    Student(const Student& other);
+    Student& operator=(const Student& other);
+    ~Student();
 
     double finalByAverage() const;
     double finalByMedian() const;
-
-    std::string toOutputString() const; // single-line representation used for writing results
-
-    // Randomly generate student data
     void randomize(unsigned int seed, int hwCount, double minVal, double maxVal);
 };
-
-inline bool studentNameCompare(const Student& a, const Student& b) {
-    if (a.surname != b.surname) return a.surname < b.surname;
-    return a.name < b.name;
-}
 
 #endif // STUDENT_H
