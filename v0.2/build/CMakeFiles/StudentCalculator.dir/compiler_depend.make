c59fbff9eb511f2dfc9733a82538aee431aa4ea7
adeolaibrahim <adeolaibrahim25@gmail.com>
2026-01-11 00:29:27 +0000

    Release v0.2: refactor structure, add file generation, exceptions, performance tools

---
+++ v0.2/build/CMakeFiles/StudentCalculator.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for StudentCalculator.
+# This may be replaced when dependencies are built.
--- v0.25/include/Generator.h
+++
@@ -1,8 +0,0 @@
-#ifndef GENERATOR_H
-#define GENERATOR_H
-
-#include <string>
-
-void generateStudentFile(const std::string& filename, size_t count, int hwCount);
-
-#endif // GENERATOR_H
