e245448d06d3c9f5bddc3c9f8e77b9d1bb81f36d
adeolaibrahim <adeolaibrahim25@gmail.com>
2026-01-11 02:09:23 +0200

    Update Student.cpp

--- v0.2/src/Student.cpp
+++ v0.2/src/Student.cpp
@@ -1,47 +1,35 @@
+// Student.cpp
 #include "Student.h"
-#include <numeric>
 #include <algorithm>
-#include <sstream>
-#include <iomanip>
-#include <random>
+#include <numeric>
+#include <cstdlib>
 
+Student::Student() : name(""), surname(""), exam(0.0) {}
 Student::Student(const std::string& n, const std::string& s, const std::vector<double>& hw, double e)
     : name(n), surname(s), homeworks(hw), exam(e) {}
+Student::Student(const Student& other) = default;
+Student& Student::operator=(const Student& other) = default;
+Student::~Student() = default;
 
 double Student::finalByAverage() const {
-    double hwAverage = 0.0;
-    if (!homeworks.empty()) {
-        hwAverage = std::accumulate(homeworks.begin(), homeworks.end(), 0.0) / homeworks.size();
-    }
+    double hwAverage = homeworks.empty() ? 0.0 :
+        std::accumulate(homeworks.begin(), homeworks.end(), 0.0) / homeworks.size();
     return 0.4 * hwAverage + 0.6 * exam;
 }
 
 double Student::finalByMedian() const {
-    if (homeworks.empty()) {
-        return 0.6 * exam;
-    }
+    if (homeworks.empty()) return 0.6 * exam;
     std::vector<double> hwCopy = homeworks;
     std::sort(hwCopy.begin(), hwCopy.end());
     size_t n = hwCopy.size();
-    double median = (n % 2 == 0) ? (hwCopy[n/2 - 1] + hwCopy[n/2]) / 2.0 : hwCopy[n/2];
+    double median = (n % 2 == 0) ? (hwCopy[n/2-1] + hwCopy[n/2])/2.0 : hwCopy[n/2];
     return 0.4 * median + 0.6 * exam;
 }
 
-std::string Student::toOutputString() const {
-    std::ostringstream oss;
-    oss << name << ' ' << surname << ' ' << std::fixed << std::setprecision(2) << finalByAverage();
-    return oss.str();
-}
-
-// Randomize homework and exam grades
 void Student::randomize(unsigned int seed, int hwCount, double minVal, double maxVal) {
-    std::mt19937 rng(seed);
-    std::uniform_real_distribution<double> dist(minVal, maxVal);
-
-    homeworks.resize(hwCount);
-    for (int i = 0; i < hwCount; ++i) {
-        homeworks[i] = dist(rng);
-    }
-
-    exam = dist(rng);
+    std::srand(seed);
+    homeworks.clear();
+    for (int i = 0; i < hwCount; ++i)
+        homeworks.push_back(minVal + (std::rand() / (double)RAND_MAX) * (maxVal - minVal));
+    exam = minVal + (std::rand() / (double)RAND_MAX) * (maxVal - minVal);
 }
